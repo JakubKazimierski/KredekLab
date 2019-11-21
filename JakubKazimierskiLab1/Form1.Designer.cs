51f0fed5fc42497c7b836b06e0ed52cd4ef363c1
JakubKazimierski <j.m.kazimierski@gmail.com>
2019-11-21 19:35:30 +0100

    Added buttons to operate timer, and to change colors of Timer, but activity of functions (events) isn't implemented yet

--- JakubKazimierskiLab1/Form1.Designer.cs
+++ JakubKazimierskiLab1/Form1.Designer.cs
@@ -29,6 +29,20 @@
         private void InitializeComponent()
         {
             this.LabelWithName = new System.Windows.Forms.Label();
+            this.TableofTimer = new System.Windows.Forms.TableLayoutPanel();
+            this.Hours = new System.Windows.Forms.Label();
+            this.MinutesTable = new System.Windows.Forms.Label();
+            this.SecondsTable = new System.Windows.Forms.Label();
+            this.HunSecondsTable = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.TableofTimer.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // LabelWithName
@@ -43,15 +57,152 @@
             this.LabelWithName.Text = "Jakub Kazimierski";
             this.LabelWithName.Click += new System.EventHandler(this.label1_Click);
             // 
+            // TableofTimer
+            // 
+            this.TableofTimer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TableofTimer.ColumnCount = 4;
+            this.TableofTimer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TableofTimer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TableofTimer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TableofTimer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TableofTimer.Controls.Add(this.HunSecondsTable, 3, 0);
+            this.TableofTimer.Controls.Add(this.SecondsTable, 2, 0);
+            this.TableofTimer.Controls.Add(this.MinutesTable, 1, 0);
+            this.TableofTimer.Controls.Add(this.Hours, 0, 0);
+            this.TableofTimer.Location = new System.Drawing.Point(21, 72);
+            this.TableofTimer.Name = "TableofTimer";
+            this.TableofTimer.RowCount = 1;
+            this.TableofTimer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TableofTimer.Size = new System.Drawing.Size(365, 100);
+            this.TableofTimer.TabIndex = 1;
+            // 
+            // Hours
+            // 
+            this.Hours.AutoSize = true;
+            this.Hours.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Hours.Location = new System.Drawing.Point(3, 0);
+            this.Hours.Name = "Hours";
+            this.Hours.Size = new System.Drawing.Size(85, 100);
+            this.Hours.TabIndex = 0;
+            this.Hours.Text = "label1";
+            // 
+            // MinutesTable
+            // 
+            this.MinutesTable.AutoSize = true;
+            this.MinutesTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MinutesTable.Location = new System.Drawing.Point(94, 0);
+            this.MinutesTable.Name = "MinutesTable";
+            this.MinutesTable.Size = new System.Drawing.Size(85, 100);
+            this.MinutesTable.TabIndex = 1;
+            this.MinutesTable.Text = "label1";
+            // 
+            // SecondsTable
+            // 
+            this.SecondsTable.AutoSize = true;
+            this.SecondsTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SecondsTable.Location = new System.Drawing.Point(185, 0);
+            this.SecondsTable.Name = "SecondsTable";
+            this.SecondsTable.Size = new System.Drawing.Size(85, 100);
+            this.SecondsTable.TabIndex = 2;
+            this.SecondsTable.Text = "label1";
+            // 
+            // HunSecondsTable
+            // 
+            this.HunSecondsTable.AutoSize = true;
+            this.HunSecondsTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HunSecondsTable.Location = new System.Drawing.Point(276, 0);
+            this.HunSecondsTable.Name = "HunSecondsTable";
+            this.HunSecondsTable.Size = new System.Drawing.Size(86, 100);
+            this.HunSecondsTable.TabIndex = 3;
+            this.HunSecondsTable.Text = "label1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tableLayoutPanel1.Controls.Add(this.StopButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.PauseButton, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.StartButton, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(21, 178);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(362, 100);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StartButton.Location = new System.Drawing.Point(3, 3);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(114, 94);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PauseButton.Location = new System.Drawing.Point(123, 3);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(114, 94);
+            this.PauseButton.TabIndex = 1;
+            this.PauseButton.Text = "Pause";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            // 
+            // StopButton
+            // 
+            this.StopButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StopButton.Location = new System.Drawing.Point(243, 3);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(116, 94);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.ColorButton, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(21, 284);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(359, 100);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ColorButton.Location = new System.Drawing.Point(3, 3);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(353, 94);
+            this.ColorButton.TabIndex = 0;
+            this.ColorButton.Text = "Change Color";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(398, 430);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.TableofTimer);
             this.Controls.Add(this.LabelWithName);
-            this.MinimumSize = new System.Drawing.Size(414, 0);
+            this.MinimumSize = new System.Drawing.Size(414, 38);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.TableofTimer.ResumeLayout(false);
+            this.TableofTimer.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -59,6 +210,17 @@
         #endregion
 
         private System.Windows.Forms.Label LabelWithName;
+        private System.Windows.Forms.TableLayoutPanel TableofTimer;
+        private System.Windows.Forms.Label HunSecondsTable;
+        private System.Windows.Forms.Label SecondsTable;
+        private System.Windows.Forms.Label MinutesTable;
+        private System.Windows.Forms.Label Hours;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button ColorButton;
     }
 }
 
--- JakubKazimierskiLab1/Form1.cs
+++ JakubKazimierskiLab1/Form1.cs
@@ -19,6 +19,16 @@ namespace JakubKazimierskiLab1
 
         private void label1_Click(object sender, EventArgs e)
         {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
 
         }
     }
