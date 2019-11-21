deaf864bd82839a85b8ad7282e127e0721ceb2dd
JakubKazimierski <j.m.kazimierski@gmail.com>
2019-11-21 17:52:50 +0100

    Added label with name, and also set minimum size of label

--- JakubKazimierskiLab1/Form1.Designer.cs
+++ JakubKazimierskiLab1/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LabelWithName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LabelWithName
+            // 
+            this.LabelWithName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LabelWithName.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F);
+            this.LabelWithName.Location = new System.Drawing.Point(12, 9);
+            this.LabelWithName.Name = "LabelWithName";
+            this.LabelWithName.Size = new System.Drawing.Size(374, 60);
+            this.LabelWithName.TabIndex = 0;
+            this.LabelWithName.Text = "Jakub Kazimierski";
+            this.LabelWithName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(398, 430);
+            this.Controls.Add(this.LabelWithName);
+            this.MinimumSize = new System.Drawing.Size(414, 0);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LabelWithName;
     }
 }
 
--- JakubKazimierskiLab1/Form1.cs
+++ JakubKazimierskiLab1/Form1.cs
@@ -16,5 +16,10 @@ namespace JakubKazimierskiLab1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
